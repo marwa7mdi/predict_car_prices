83c80a7c20abc32b3407ccc970d888dd7bf1751d
marwa7mdi <64111446+marwa7mdi@users.noreply.github.com>
2020-10-31 19:53:58 +0200

    readme

---
+++ readme.mk
@@ -0,0 +1,6 @@
+Machine learning project to predict car price, first analyze and visualize the dataset to
+understand the data and the relation between features. Second predict car price using
+three models LinearRegression, DecisionTreeRegressor, RandomForestRegressor, results
+with accuracy 92%.Finaly deploy the model using flask.
+App link:
+https://eg-predict-car-price.herokuapp.com/?fbclid=IwAR3ITuIDJAXVdZulYlVF6pkvPYTIYFGznOZkURLBJuJke-373qd5pRbSCM
